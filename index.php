78ee9cddd84ab055c52f144790c0691f74823a2b
kochengpoi <4duuuh@gmail.com>
2021-10-15 07:23:31 +0700

    hehe

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" href="favicon.png">
+	<title>Web Prak</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/style.css">
+	<style>
+</style> 
+</head>
+<body>
+	<header>
+		<nav>
+			<ul>
+				<li class="logo"><h2>Pemrograman Web</h2></li>
+				<div class="nav">
+					<li class="active"><a href="">Home</a></li>
+					<li><a href="">Shop</a></li>
+					<li><a href="">Mail</a></li>
+					<li><a href="">Contact Us</a></li>
+					<li><a href="">About</a></li>
+				</div>
+			</ul>
+		</nav>
+		<div class="kotak" style="font-family: 'Righteous', cursive;" >
+			<h2>			
+				<b >Menuju tak terbatas, dan melampauinya..!</b> 
+			</h2>
+			<br>
+			<a href="" class="btn btn-info"><h1>Selesaikan misi..!!</h1></a>
+		</div>
+	</header>
+	<div class="container">
+		<div class="row">
+			<div class="col">
+
+			</div>
+		</div>
+	</div>
+</body>
+</html>
